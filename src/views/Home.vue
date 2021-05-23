38a49c827e9d8909da6578a8752b66bbe902ce6c
1281055751 <1281055751@qq.com>
2021-05-23 13:30:51 +0800

    d

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
+    <img src="">
+    <div id="demo"></div>
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
@@ -16,3 +19,10 @@ export default {
   }
 }
 </script>
+<style>
+  #demo{
+    width: 100px;
+    height: 100px;
+    background:url("~assets/images/logo.png") no-repeat;
+  }
+</style>
