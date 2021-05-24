1367a1c3acc1407b5eec4587b7c28f37adfa2dcf
1281055751 <1281055751@qq.com>
2021-05-24 10:23:20 +0800

    2021.5.24

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+const Home = () => import( '../views/Home')
+const Category = () => import( '../views/category/Category')
+const Detail = () => import( '../views/detail/Detail')
+const Profile = () => import( '../views/profile/Profile')
+const ShopCart = () => import( '../views/shopcart/ShopCart')
+const routes = [
+    {
+        path: '',
+        name: 'DefaultHome',
+        component: Home
+    },
+    {
+        path: '/category',
+        name: 'Category',
+        component: Category
+    },
+    {
+        path: '/detail',
+        name: 'Detail',
+        component: Detail
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: Profile
+    },
+    {
+        path: '/shopcart',
+        name: 'ShopCart',
+        component: ShopCart
+    },
+
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    {{ banner }}
+  </div>
+</template>
+
+<script>
+import {ref, onMounted} from "vue";
+import {getHomeAllData} from "@/network/home";
+import {getBanner} from "@/network/home";
+
+export default {
+  name: 'Home',
+  components: {},
+  setup() {
+    const banner = ref([])
+
+    onMounted(() => {
+      console.log('111')
+      getHomeAllData().then(res => {
+
+        banner.value = res.slides;
+
+
+      }).catch(err => {
+
+      })
+    })
+    return {
+      banner
+    }
+  }
+}
+</script>
+<style>
+#demo {
+  width: 100px;
+  height: 100px;
+  background: url("~assets/images/logo.png") no-repeat;
+}
+</style>
